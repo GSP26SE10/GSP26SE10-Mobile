5419033f2f31a74443fef72a44e7cb93c173fac0
taivippro123 <phanvothanhtai1007@gmail.com>
2025-12-25 16:06:13 +0700

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { Button } from "react-native";
 
 export default function App() {
+  const [count, setCount] = React.useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ marginBottom: 20 }}>You pressed {count} times</Text>
+      <Button style={{ marginBottom: 20 }} title="Press me" onPress={() => setCount(count + 1)} />
+      <Button title="Reset" onPress={() => setCount(0)} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
